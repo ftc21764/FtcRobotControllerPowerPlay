8e2320f87204473e3dece99f58b787052e56b6ac
ast_lpond <l.pond@astoundcommerce.com>
2022-09-29 17:27:30 -0700

    Save work in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Drivetrain.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Drivetrain {
@@ -11,9 +12,10 @@ public class Drivetrain {
     }
 
     State state = State.IDLE;
+    DcMotor leftMotor;
 
     public void init(HardwareMap hardwareMap) {
-
+        leftMotor = hardwareMap.get(DcMotor.class, "left_motor");
     }
 
     public void loop() {
